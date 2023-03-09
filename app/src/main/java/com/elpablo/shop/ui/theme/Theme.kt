01c5e8ccfb874280e4c8a1e4841306d86e36de45
elpablo <elpablo.volgograd@yandex.ru>
2023-03-09 20:28:25 +0300

    typography and color

--- app/src/main/java/com/elpablo/shop/ui/theme/Theme.kt
+++ app/src/main/java/com/elpablo/shop/ui/theme/Theme.kt
@@ -16,15 +16,19 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = ButtonColor,
+    secondary = SecondaryTextColor,
+    tertiary = TextHintColor,
+    background = BackgroundColor,
+    surface = MainTextColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = ButtonColor,
+    secondary = SecondaryTextColor,
+    tertiary = TextHintColor,
+    background = BackgroundColor,
+    surface = MainTextColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
--- app/src/main/java/com/elpablo/shop/ui/theme/Type.kt
+++ app/src/main/java/com/elpablo/shop/ui/theme/Type.kt
@@ -14,21 +14,54 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 17.sp,
+        letterSpacing = 0.3.sp
+    ),
+    bodySmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 9.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 0.3.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 26.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.3.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.3.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.3.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = 15.sp,
+        letterSpacing = 0.3.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 8.sp,
+        lineHeight = 12.sp,
+        letterSpacing = 0.3.sp
     )
-    */
 )
